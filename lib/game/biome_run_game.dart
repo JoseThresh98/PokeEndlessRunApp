f22f515381269d4c1c19b7c76ecd8c821fd306a2
Jose Antonio Aguilar <joseaguilar0798@gmail.com>
2026-03-02 09:19:39 -0600

    Core Game Engine
    add core game engine, player, world and HUD components

---
+++ lib/game/biome_run_game.dart
@@ -0,0 +1,86 @@
+import 'package:flame/game.dart';
+import 'package:flame/events.dart';
+import '../core/constants/app_constants.dart';
+import '../data/repositories/score_repository.dart';
+import 'components/world_component.dart';
+import 'components/player_component.dart';
+import 'components/hud_component.dart';
+
+class BiomeRunGame extends FlameGame with TapCallbacks {
+
+  late PlayerComponent player;
+  late WorldComponent gameWorld;
+  late HudComponent hud;
+
+  final ScoreRepository scoreRepository;
+
+  int currentScore = 0;
+  int sessionCoins = 0;
+  int sessionGems = 0;
+  bool isGameOver = false;
+
+  double _gameSpeed = AppConstants.gameSpeed;
+  double get gameSpeed => _gameSpeed;
+
+  BiomeRunGame({required this.scoreRepository});
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    gameWorld = WorldComponent();
+    await add(gameWorld);
+
+    player = PlayerComponent();
+    await add(player);
+
+    hud = HudComponent(game: this);
+    await add(hud);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (isGameOver) return;
+
+    currentScore += (dt * AppConstants.scorePerMeter * _gameSpeed / 100).round();
+    _gameSpeed += AppConstants.speedIncrement * dt;
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    if (!isGameOver) {
+      player.jump();
+    }
+  }
+
+  void collectCoin() {
+    sessionCoins += AppConstants.coinValue;
+    currentScore += AppConstants.coinValue;
+  }
+
+  void collectGem() {
+    sessionGems += AppConstants.gemValue;
+    currentScore += AppConstants.gemValue;
+  }
+
+  Future<void> triggerGameOver() async {
+    isGameOver = true;
+    pauseEngine();
+
+    await scoreRepository.updateHighScore(currentScore);
+    await scoreRepository.addCoins(sessionCoins);
+    await scoreRepository.addGems(sessionGems);
+  }
+
+  void restartGame() {
+    currentScore = 0;
+    sessionCoins = 0;
+    sessionGems = 0;
+    isGameOver = false;
+    _gameSpeed = AppConstants.gameSpeed;
+    resumeEngine();
+    removeAll(children);
+    onLoad();
+  }
+}
---
+++ lib/game/components/player_component.dart
@@ -0,0 +1,130 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import '../../core/constants/app_constants.dart';
+import '../../core/constants/creature_type.dart';
+import '../../core/theme/app_colors.dart';
+
+class PlayerComponent extends RectangleComponent {
+  static const double groundY = 436.0;
+
+  double _velocityY = 0;
+  bool _isOnGround = true;
+  bool _isAbilityActive = false;
+  double _abilityCooldownTimer = 0;
+
+  CreatureType currentType = CreatureType.fire;
+
+  PlayerComponent()
+      : super(
+    size: Vector2(64, 64),
+    position: Vector2(100, groundY),
+    paint: Paint()..color = AppColors.fireType,
+  );
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    // Gravity
+    if (!_isOnGround) {
+      _velocityY += AppConstants.gravity * dt;
+      position.y += _velocityY * dt;
+
+      if (position.y >= groundY) {
+        position.y = groundY;
+        _velocityY = 0;
+        _isOnGround = true;
+      }
+    }
+
+    // Ability cooldown
+    if (_abilityCooldownTimer > 0) {
+      _abilityCooldownTimer -= dt;
+    } else {
+      _isAbilityActive = false;
+    }
+  }
+
+  void jump() {
+    if (_isOnGround) {
+      _velocityY = AppConstants.jumpForce;
+      _isOnGround = false;
+    }
+  }
+
+  void useAbility() {
+    if (_abilityCooldownTimer > 0) return;
+
+    _isAbilityActive = true;
+    _abilityCooldownTimer = _getCooldown();
+
+    switch (currentType) {
+      case CreatureType.fire:
+        _fireDash();
+        break;
+      case CreatureType.water:
+        _waterGlide();
+        break;
+      case CreatureType.psychic:
+        _psychicTeleport();
+        break;
+      case CreatureType.grass:
+        _grassVineSwing();
+        break;
+    }
+  }
+
+  void swapCreature(CreatureType type) {
+    currentType = type;
+    paint.color = _getTypeColor();
+  }
+
+  void _fireDash() {
+    position.x += 80;
+  }
+
+  void _waterGlide() {
+    _velocityY = AppConstants.jumpForce * 0.5;
+    _isOnGround = false;
+  }
+
+  void _psychicTeleport() {
+    position.x += 120;
+  }
+
+  void _grassVineSwing() {
+    _velocityY = AppConstants.jumpForce * 0.75;
+    _isOnGround = false;
+  }
+
+  double _getCooldown() {
+    switch (currentType) {
+      case CreatureType.fire:
+        return AppConstants.fireDashCooldown;
+      case CreatureType.water:
+        return AppConstants.waterGlideCooldown;
+      case CreatureType.psychic:
+        return AppConstants.psychicTeleportCooldown;
+      case CreatureType.grass:
+        return AppConstants.grassVineCooldown;
+    }
+  }
+
+  Color _getTypeColor() {
+    switch (currentType) {
+      case CreatureType.fire:
+        return AppColors.fireType;
+      case CreatureType.water:
+        return AppColors.waterType;
+      case CreatureType.psychic:
+        return AppColors.psychicType;
+      case CreatureType.grass:
+        return AppColors.grassType;
+    }
+  }
+
+  double get abilityCooldownPercent =>
+      _abilityCooldownTimer / _getCooldown();
+
+  bool get isAbilityReady => _abilityCooldownTimer <= 0;
+}
